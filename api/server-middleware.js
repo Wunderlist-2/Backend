4b9f6ae9d43a8a4b5970b7e2807ca5f234d2bc67
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-03 20:07:39 -0800

    autosave cookie

--- api/server-middleware.js
+++ api/server-middleware.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const session = require("express-session");
-const { name, secret, node_env } = require("../config");
-const helmet = require("helmet");
-const cors = require("cors");
+const express = require('express')
+const session = require('express-session')
+const { name, secret, node_env } = require('../config')
+const helmet = require('helmet')
+const cors = require('cors')
 
-const logger = require("./logger");
+const logger = require('./logger')
 
 const sessionConfig = {
   name: name,
   secret: secret,
   cookie: {
     maxAge: 1000 * 60 * 60, //60 minutes
-    secure: node_env === "production" ? true : false,
-    httpOnly: true
+    secure: node_env === 'production' ? true : false,
+    httpOnly: true,
   },
   resave: false,
-  saveUninitialized: false //GDPR laws against setting cookies auto
-};
+  saveUninitialized: true,
+}
 
 module.exports = server => {
-  server.use(helmet());
-  server.use(express.json());
-  server.use(cors());
-  server.use(session(sessionConfig));
-  server.use(logger);
-};
+  server.use(helmet())
+  server.use(express.json())
+  server.use(cors())
+  server.use(session(sessionConfig))
+  server.use(logger)
+}
